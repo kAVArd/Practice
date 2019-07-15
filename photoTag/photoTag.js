8e2d6c0467bf868bc4029bed9fa73ef1aeb2276b
kAVArd <savinovilja1999@gmail.com>
2019-07-15 11:56:33 +0300

    add tag source (like a one source of truth)

--- photoTag/photoTag.js
+++ photoTag/photoTag.js
@@ -1,49 +1,81 @@
 const inputWrapper = document.getElementById('input-wrapper')
-const position = { x: 0, y: 0 }
+const position = { x: '', y: '' }
 const tagInput = document.getElementById('tag-input')
 inputWrapper.style.visibility = 'hidden'
 let drugObject = null
 
+const tagSource = {
+  tagArray: [],
+  editing: null,
+  printTags: () => {
+    tagSource.tagArray.forEach((tag, index) => {
+      if (!tag.isPrinted) {
+        const tagSpan = document.createElement('span')
+        tagSpan.setAttribute('class', tag.class)
+        tagSpan.id = index
+        tagSpan.innerHTML = tag.text
+        tagSpan.style.left = tag.position.x
+        tagSpan.style.top = tag.position.y
+
+        tagSpan.onmousedown = function () {
+          drugObject = this
+        }
+        tagSpan.onmouseup = () => {
+          drugObject = null
+        }
+        tagSpan.ondblclick = tagToInput
+
+        tag.element = tagSpan
+        document.getElementById('tags-wrapper').appendChild(tagSpan)
+        tag.isPrinted = true
+      }
+      if (tag.isEditing) {
+        tag.element.style.visibility = 'hidden'
+      } else {
+        tag.element.style.visibility = 'visible'
+      }
+    })
+  },
+  deleteTag: (id) => {
+    if (id === null) return
+    tagSource.tagArray[id].element.remove()
+    tagSource.tagArray.splice(id, 1)
+  }
+}
+
+function Tag (text = '', position = { x: 0, y: 0 }) {
+  this.text = `<h6>${text}</h6>`
+  this.isEditing = false
+  this.position = position
+  this.isPrinted = false
+  this.class = 'tag badge badge-secondary'
+  this.isVisible = true
+  this.element = {}
+}
+
 const createTag = (e) => {
   if (e && e.target.id === 'image' && inputWrapper.style.visibility === 'hidden') {
-    position.x = e.clientX
-    position.y = e.clientY
+    position.x = e.clientX - inputWrapper.offsetWidth / 2 + 'px'
+    position.y = e.clientY - inputWrapper.offsetHeight / 2 + 'px'
 
-    inputWrapper.style.position = 'absolute'
-    inputWrapper.style.left = position.x + 'px'
-    inputWrapper.style.top = position.y + 'px'
+    inputWrapper.style.left = position.x
+    inputWrapper.style.top = position.y
     inputWrapper.style.visibility = 'visible'
     tagInput.focus()
     return
   }
 
-  const textTag = tagInput.value
-  tagInput.value = ''
-  inputWrapper.style.visibility = 'hidden'
-
-  if (textTag === '') {
+  if (tagInput.value === '') {
     return
   }
 
-  const tagSpan = document.createElement('span')
-  tagSpan.setAttribute('class', 'badge badge-secondary')
-  tagSpan.id = 'tag'
-  tagSpan.innerHTML = `<h6>${textTag}</h6>`
-  tagSpan.style.position = 'absolute'
-  tagSpan.style.left = position.x + 'px'
-  tagSpan.style.top = position.y + 'px'
+  if (tagSource.editing !== null) {
+    tagSource.tagArray[tagSource.editing].isEditing = false
+  } else tagSource.tagArray.push(new Tag(tagInput.value, { x: position.x, y: position.y }))
 
-  tagSpan.onmousedown = function (e) {
-    drugObject = this
-  }
-
-  tagSpan.onmouseup = (e) => {
-    drugObject = null
-  }
-
-  tagSpan.ondblclick = tagToInput
-
-  document.getElementById('tags-wrapper').appendChild(tagSpan)
+  tagInput.value = ''
+  inputWrapper.style.visibility = 'hidden'
+  tagSource.printTags()
 }
 document.body.addEventListener('click', createTag)
 
@@ -61,20 +93,23 @@ document.getElementById('image').onmousemove = function (e) {
 }
 
 const tagToInput = (e) => {
-  console.dir(e)
-  const text = e.target.textContent
-  position.x = e.clientX - e.offsetWidth
-  position.y = e.clientY - e.offsetHeight
-  e.target.parentNode.remove()
-  inputWrapper.style.left = position.x + 'px'
-  inputWrapper.style.top = position.y + 'px'
+  const tagSpan = e.target.parentElement
+  tagInput.value = tagSpan.textContent
+  position.x = tagSpan.offsetLeft + 'px'
+  position.y = tagSpan.offsetTop + 'px'
+  tagSource.tagArray[tagSpan.id].isEditing = true
+  tagSource.editing = tagSpan.id
+
+  inputWrapper.style.left = position.x
+  inputWrapper.style.top = position.y
   inputWrapper.style.visibility = 'visible'
-  tagInput.value = text
 
   tagInput.focus()
+  tagSource.printTags()
 }
 
 document.getElementById('close').addEventListener('click', (e) => {
+  tagSource.deleteTag(tagSource.editing)
   tagInput.value = ''
   inputWrapper.style.visibility = 'hidden'
 })
